9a0f0a1d20234bdf83d741e236b0e4a3b48e994c
anis31410 <anissemahtat@yahoo.com>
2019-08-27 14:36:24 +0200

    un input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AuthComponent } from './auth/auth.component';
+import { InputComponent } from './input/input.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AuthComponent ],
+  declarations: [ AppComponent, HelloComponent, AuthComponent, InputComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
